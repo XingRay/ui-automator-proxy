f676a4797dabd97d8d79002643b9697d659e4fca
XingRay <leixing1012@gmail.com>
2024-09-27 20:49:17 +0800

    1. 开放 hierarchy 的返回字段

--- app/src/main/java/com/github/xingray/uiautomatorproxy/device/Device.kt
+++ app/src/main/java/com/github/xingray/uiautomatorproxy/device/Device.kt
@@ -5,12 +5,10 @@ import android.graphics.Bitmap
 import android.graphics.Rect
 import android.util.Log
 import android.view.accessibility.AccessibilityNodeInfo
-import androidx.test.platform.app.InstrumentationRegistry
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.uiautomator.AccessibilityNodeInfoDumper
 import com.github.xingray.uiautomatorproxy.androidtest.AndroidTestHolder
 import com.github.xingray.uiautomatorproxy.kotlin.trueOrNull
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.OutputStream
@@ -178,76 +176,6 @@ class Device() {
         }
 
         return node
-    }
-
-
-    fun screenshot() {
-        val params = HashMap<String, String>()
-        var scale = 1.0f
-        if (params.containsKey("scale")) {
-            try {
-                scale = params.get("scale")?.toFloat() ?: 1.0f
-            } catch (e: NumberFormatException) {
-            }
-        }
-        var quality = 100
-        if (params.containsKey("quality")) {
-            try {
-                quality = params.get("quality")?.toInt() ?: 100
-            } catch (e: NumberFormatException) {
-            }
-        }
-
-
-        //            File f = new File(InstrumentationRegistry.getTargetContext().getFilesDir(), "screenshot.png");
-//            UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-//            uiDevice.takeScreenshot(f, scale, quality);
-
-//            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        val instrumentation = InstrumentationRegistry.getInstrumentation()
-        val uiAutomation = instrumentation.uiAutomation
-
-        val start1 = System.currentTimeMillis()
-        val screenshot = uiAutomation.takeScreenshot()
-        val time1 = System.currentTimeMillis() - start1
-        Log.d(TAG, "serve: time1: $time1")
-
-
-        //            long start2 = System.currentTimeMillis();
-//            screenshot.compress(Bitmap.CompressFormat.JPEG, quality, bos);
-//            long time2 = System.currentTimeMillis() - start2;
-        val start3 = System.currentTimeMillis()
-        val width = screenshot.width
-        val height = screenshot.height
-        val pixels = IntArray(width * height)
-        screenshot.getPixels(pixels, 0, width, 0, 0, width, height)
-        val time3 = System.currentTimeMillis() - start3
-        Log.d(TAG, "serve: time3: $time3")
-
-        val start4 = System.currentTimeMillis()
-
-        // 创建字节数组用于存储像素数据
-        val imageData = ByteArray(width * height * 4) // 每个像素 4 个字节 (ARGB)
-        for (i in pixels.indices) {
-            imageData[i * 4] = ((pixels[i] shr 16) and 0xFF).toByte() // R
-            imageData[i * 4 + 1] = ((pixels[i] shr 8) and 0xFF).toByte() // G
-            imageData[i * 4 + 2] = (pixels[i] and 0xFF).toByte() // B
-            imageData[i * 4 + 3] = ((pixels[i] shr 24) and 0xFF).toByte() // A
-        }
-        val time4 = System.currentTimeMillis() - start4
-        Log.d(TAG, "serve: time4: $time4")
-
-
-        //
-//            try {
-//                bos.flush();
-//            } catch (IOException e) {
-//                Log.e(e.getMessage());
-//                return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "Internal Server Error!!!");
-//            }
-//            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "Internal Server Error!!!, time1:" + time1);
-        val byteArrayInputStream = ByteArrayInputStream(imageData)
-//        return newChunkedResponse(Response.Status.OK, "application/octet-stream", byteArrayInputStream)
     }
 
     fun writeScreenShot(androidTestHolder: AndroidTestHolder, outputStream: OutputStream, format: String, quality: Int = 100) {
